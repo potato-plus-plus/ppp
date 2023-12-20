7ef1ff493db13a435bb36e6f6ec937ec24470adb
ModragX <151087186+qazox@users.noreply.github.com>
2023-12-20 18:04:11 -0500

    resarch clicker

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,138 @@
 <script>
-    import Button from "$lib/Button.svelte";
+    let technologicalProgression = "Stone";
+
+    import { Currency, Factory } from "$lib/clicker.js";
+    import CurrencyItem from "$lib/CurrencyItem.svelte";
+    import FactoryItem from "$lib/FactoryItem.svelte";
+
+    let causeUpdate = () => {
+        currencies = { ...currencies };
+        upgrades = { ...upgrades };
+        technology.stoneTools.invis = !(upgrades.scientist.amount > 9);
+    };
+
+    let currencies = {
+        research: new Currency("TECH", causeUpdate, "research"),
+        land: new Currency("LAND", causeUpdate, "land"),
+        stone: new Currency("ROCK", causeUpdate, "stone"),
+        wood: new Currency("WOOD", causeUpdate, "wood"),
+        dirt: new Currency("DIRT", causeUpdate, "dirt"),
+    };
+
+    currencies.research.perClick = 1;
+    currencies.stone.delay = 3000;
+
+    let upgrades = {
+        scientist: new Factory(
+            "Scientist",
+            {
+                research: 15,
+            },
+            {
+                research: {
+                    perClick: 0,
+                    delay: 1,
+                    perDelay: 1,
+                },
+            },
+            1.1,
+            causeUpdate,
+            false,
+            "scientist"
+        ),
+        miner: new Factory(
+            "Miner",
+            {
+                stone: 10,
+            },
+            {
+                stone: {
+                    perClick: 0,
+                    delay: 1,
+                    perDelay: 1,
+                },
+            },
+            1.1,
+            causeUpdate,
+            false,
+            "miner"
+        ),
+    };
+    let technology = {
+        rockfinding: new Factory(
+            "Rock Finding",
+            {
+                research: 15,
+            },
+            {
+                stone: {
+                    perClick: 0.1,
+                    delay: 1,
+                    perDelay: 0,
+                },
+            },
+            1.1,
+            causeUpdate,
+            true,
+            "stone"
+        ),
+        stoneTools: new Factory(
+            "Stone Tools",
+            {
+                research: 100,
+                stone: 50,
+            },
+            {
+                stone: {
+                    perClick: 0.1,
+                    delay: 1,
+                    perDelay: 0,
+                },
+            },
+            1.1,
+            causeUpdate,
+            true,
+            "research"
+        ),
+    };
 </script>
 
-<section class="left">
-    <h1 class="row">
-        <img src="/potatoplusplus.svg" alt="icon" />
-        <p>Welcome to Potato++</p>
-    </h1>
-    <p>
-        Potato++ is a website where two brothers make games and put them here so
-        people can play them.
-    </p>
-    <section class="row">
-        <Button href="https://discord.gg/KnAU3Snc9g" content="Discord" />
-        <Button
-            href="https://github.com/potato-plus-plus/ppp"
-            content="Source Code"
-        />
-    </section>
-    <h1>Games</h1>
-    <h2>Early Access Games</h2>
-    <p>These games are games that are not finished yet, but are available to play.</p>
-    <section class="row">
-        <Button
-            href="researchclicker"
-            content="Research Clicker"
-            icon="/research.svg"
-        />
-        <Button
-            href="roguepotato"
-            content="Rogue Potato"
-            icon="/research.svg"
-        />
-    </section>
+<h1>{technologicalProgression} Age</h1>
+<a
+    on:click={() => {
+        for (let currency in currencies) {
+            currencies[currency].click();
+        }
+    }}><img src="/research.svg" /></a
+>
+<p>Click to <b>Research</b></p>
 
-    <h2>Finished Games</h2>
-    <p>These games are full games, and are no longer in the Early Access phase.</p>
+<h2>Raw Resources</h2>
 
-    <h1>Credits</h1>
-    <section class="row">
-        <Button href="https://github.com/thepotatoplus" content="Potato" />
-        <Button href="https://github.com/nanodev-x" content="NDX" />
-    </section>
+<section class="row">
+    {#each Object.values(currencies) as currency}
+        {#if !currency.invis}
+            <CurrencyItem {currency} />
+        {/if}
+    {/each}
+</section>
+
+<h2>Generators</h2>
+
+<section class="row">
+    {#each Object.values(upgrades) as upgrade}
+        {#if !upgrade.invis}
+            <FactoryItem factory={upgrade} {currencies} />
+        {/if}
+    {/each}
+</section>
+
+<h2>Research</h2>
+
+<section class="row">
+    {#each Object.values(technology) as upgrade}
+        {#if !upgrade.invis}
+            <FactoryItem factory={upgrade} {currencies} />
+        {/if}
+    {/each}
 </section>
